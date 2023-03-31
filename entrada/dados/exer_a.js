fd57446dc0a3a3e4415b66a9441491a0918bc126
Paulo Myckael <107208050+PauloMyckael@users.noreply.github.com>
2023-03-31 04:22:32 -0300

    Create exer_a.js

---
+++ entrada/dados/exer_a.js
@@ -0,0 +1,9 @@
+// Lê os dados de entrada
+let num = Number(prompt("Número: "));
+
+let anterior = (num - 1);
+let posterior = (num + 2);
+
+// conclusão
+
+alert("Número anterior: " + anterior + ", Número posterior: " + posterior);
