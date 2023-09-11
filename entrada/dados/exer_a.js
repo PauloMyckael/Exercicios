9fd6be456e6f1a46c535812feafc90b995ea5410
myckael <myckaelpaulo@gmail.com>
2023-09-11 15:57:46 -0300

    att

--- entrada/dados/exer_a.js
+++
@@ -1,9 +0,0 @@
-// Lê os dados de entrada
-let num = Number(prompt("Número: "));
-
-let anterior = (num - 1);
-let posterior = (num + 2);
-
-// conclusão
-
-alert("Número anterior: " + anterior + ", Número posterior: " + posterior);
--- getElement/scripts/script.js
+++
@@ -1,7 +0,0 @@
-function mostrarOla() {
-    let nome = document.getElementById("nome").value;
-    document.getElementById("resposta").innerHTML = "Olá " + nome; 
-}
-
-let mostrar = document.getElementById("mostrar");
-mostrar.addEventListener("click", mostrarOla);
